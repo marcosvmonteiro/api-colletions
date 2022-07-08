3405eb3a6014c31e4e67303405419f28aad7f4e3
marcosvmonteiro <marcosvmonteiro@gmail.com>
2022-07-08 13:36:45 -0300

    Created TesteList Array class

---
+++ src/main/kotlin/one/digitalinovation/colletion/TesteList.kt
@@ -0,0 +1,28 @@
+package one.digitalinovation.colletion
+
+fun main() {
+    val joao = Funcionario("João",1500.0)
+    val maria = Funcionario("Maria",12300.0)
+    val pedro = Funcionario("Pedro",5100.0)
+
+    val funcionarios = listOf(joao, pedro, maria)
+
+    funcionarios.forEach{
+        println(it)
+        println("_______________")
+    }
+    println("_______________")
+    println(funcionarios.find { it.nome == "Maria" })
+
+}
+data class Funcionario(
+            val nome: String,
+            val salario:Double
+){
+    override fun toString(): String =
+    """
+        Nome: $nome
+        Salário: $salario
+    """.trimIndent()
+
+    }
