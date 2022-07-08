99dc4af90581a89491d91a095f30c6ec52802546
marcosvmonteiro <marcosvmonteiro@gmail.com>
2022-07-08 11:14:51 -0300

    Created TesteOperações to use count find and array Array class

--- src/main/kotlin/one/digitalinovation/colletion/TesteOperacoes.kt
+++ src/main/kotlin/one/digitalinovation/colletion/TesteOperacoes.kt
@@ -16,4 +16,16 @@ fun main() {
       println("______________")
 
     salarioMaiorQue2500.forEach{ println(it) }
+    println("______________")
+
+    println(salarios.count { it in 2100.0 .. 5100.0 })
+
+    println("______________")
+
+    println(salarios.find {it ==1000.0})
+    println(salarios.find {it ==500.0})
+    println("______________")
+
+    println(salarios.any {it ==5000.0})
+    println(salarios.any {it ==510.0})
 }
